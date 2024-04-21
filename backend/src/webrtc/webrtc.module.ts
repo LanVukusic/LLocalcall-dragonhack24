7a881884826bd7644a64a3b3bb6ee874ade91fd2
Jan Vasiljevic <vasiljevic.jan@gmail.com>
2024-04-21 03:03:26 +0200

    Streaming logic

--- backend/src/webrtc/webrtc.module.ts
+++ backend/src/webrtc/webrtc.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { WebrtcGateway } from './webrtc.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Users } from 'src/users/users.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Users])],
   providers: [WebrtcGateway],
 })
 export class WebrtcModule {}
