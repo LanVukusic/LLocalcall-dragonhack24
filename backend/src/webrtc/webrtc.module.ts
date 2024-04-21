f7c437818819c99f6f63636b19a1b7b4564d7c1e
Jan Vasiljevic <vasiljevic.jan@gmail.com>
2024-04-21 03:27:05 +0200

    Even more work

--- backend/src/webrtc/webrtc.module.ts
+++ backend/src/webrtc/webrtc.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { WebrtcGateway } from './webrtc.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Users } from 'src/users/users.entity';
+import { Transcript } from 'src/transcripts/entities/transcript.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Users])],
+  imports: [TypeOrmModule.forFeature([Users, Transcript])],
   providers: [WebrtcGateway],
 })
 export class WebrtcModule {}
